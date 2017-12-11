240b15af5c9a59fb2c91ebf802405aef516d7f4a
Joker <jokers4140@gmail.com>
2017-12-11 19:17:21 +0900

    favファイルとretファイルを作成

--- fav_bot.php
+++ fav_bot.php
@@ -1,7 +1,8 @@
-<?php
 
+<?php
 // TwitterOAuth呼び出し
 require "twitteroauth/autoload.php";
+
 // api設定ファイル呼び出し
 require_once("apiconfig.php");
 
@@ -10,6 +11,22 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 //twitterと連結
 $TwitterOAuth = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
 
-    $tweets = $TwitterOAuth->post("statuses/update", ['status'=> "masterツイート"]);
 
+    $favkeyword = "アパマンショップ";   
+    $favcount = 8;
+
+
+    //キーワード検索
+    $keywords = $TwitterOAuth->get('search/tweets',array('q' => $favkeyword, 'count' => $favcount ))->statuses;
+
+
+    foreach ($keywords as $key ){
+
+        $id = $key->id;
+        $res = $TwitterOAuth->post("favorites/create",['id' => $id ]);
+        
+        // var_dump($key->id);
+    }
+        $tweets = $TwitterOAuth->post("statuses/update", ['status'=> "${favkeyword}の文字を含むツイートを${favcount}件Botによってファボしました"]);
+    
 ?>
