6c79995da00a644754ea535db52296b387904d6b
kSunaga <jokers4140@gmail.com>
2018-12-30 14:03:51 +0900

    リファクタリング

--- api_test/fav_bot.php
+++ api_test/fav_bot.php
@@ -1,35 +1,24 @@
 
 <?php
-// TwitterOAuth呼び出し
 require "../twitteroauth/autoload.php";
-
-// api設定ファイル呼び出し
 require_once("../apiconfig.php");
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-//twitterと連結
 $TwitterOAuth = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
 
-    //検索キーワード指定
     $favkeyword = "かまトゥー";
-
-    //検索数を指定
     $favcount = 50;
-    
-    //キーワード検索
+
+    //検索
     $keywords = $TwitterOAuth->get('search/tweets',array('q' => $favkeyword, 'count' => $favcount ))->statuses;
 
-    //キーワードを取得してuserIdがツイート用のアカウントだった場合、そのツイートにリプライを返す
     foreach ($keywords as $key ){
 
-        if($userId == $key->user->id_str)
-        {
+        if($userId == $key->user->id_str) {
             $id = $key->id;
             $res = $TwitterOAuth->post("favorites/create",['id' => $id ]);
             $tweets = $TwitterOAuth->post("statuses/update", array('status' => "@nihonkogakuin_ かまトゥだよ！", 'in_reply_to_status_id' => $id ));
         }
-
     }
 
-?>
--- api_test/ret_bot.php
+++ api_test/ret_bot.php
@@ -1,35 +1,22 @@
 <?php
 
-
-// TwitterOAuth呼び出し
 require "twitteroauth/autoload.php";
-
-// api設定ファイル呼び出し
 require_once("apiconfig.php");
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-
-//twitterと連結
 $TwitterOAuth = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
 
-
-    $retkeyword = "ドラえもん";   
+    $retkeyword = "ドラえもん";
     $retcount = 2;
 
-
-    //キーワード検索
+    //検索
     $keywords = $TwitterOAuth->get('search/tweets',array('q' => $retkeyword, 'count' => $retcount ))->statuses;
 
     foreach ($keywords as $key ){
-
         $id = $key->id;
         $res = $TwitterOAuth->post("statuses/retweet/${id}");
-
-        // var_dump($id);
-
     }
-        
         $tweets = $TwitterOAuth->post("statuses/update", ['status'=> "${retkeyword}の文字を含むツイートを${retcount}件Botによってリツイートしました"]);
-        
+
 ?>
